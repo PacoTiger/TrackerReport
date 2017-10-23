0bc34f8bd9887f21f9f36ad56a465c11d98016c1
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-10-23 14:58:59 +0100

    improving css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,17 +6,17 @@
     <div class="btn-group btn-group-justified" role="group" aria-label="...">
         <div class="btn-group" role="group">
             <a href="/import">
-                <button type="button" class="btn btn-default">Import</button>
+                <button type="button" class="btn btn-default btn-lg">Import</button>
             </a>
         </div>
         <div class="btn-group" role="group">
             <a href="/trackers">
-                <button type="button" class="btn btn-default">All data</button>
+                <button type="button" class="btn btn-lg btn-primary">All data</button>
             </a>
         </div>
         <div class="btn-group" role="group">
             <a href="/speakers">
-                <button type="button" class="btn btn-default">Speakers</button>
+                <button type="button" class="btn btn-lg btn-primary">Speakers</button>
             </a>
         </div>
     </div>
